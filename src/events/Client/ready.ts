ef2af12806fdcab04ddf7eb85acc234ee0d5b083
ceotocende <andreyserov03@gmail.com>
2025-12-02 22:40:52 +0300

    Хозяин

--- src/events/Client/ready.ts
+++ src/events/Client/ready.ts
@@ -1,8 +1,17 @@
 import { client as Client } from "../..";
 import { ActivityType, TextChannel } from "discord.js";
 
-Client.once('ready', (client) => {
+Client.once('ready', async (client) => {
     console.log('Logged in as: ' + Client.user?.tag);
     Client.user?.setActivity('голоса',{ type: ActivityType.Listening });
     Client.user?.setStatus("idle")
+
+    const checkChannel = client.guilds.cache.get('1397730981871620298');
+    if (!checkChannel) {
+        return
+    } else {
+        const channelSendStart = await checkChannel.channels.cache.get(checkChannel.id) as TextChannel;
+
+        channelSendStart.send('Хозяин, я проснулась!')
+    }
 });
