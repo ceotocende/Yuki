9e2b5e893d02dfe2a5531029be6c4103bc32b714
ceotocende <andreyserov03@gmail.com>
2026-01-27 22:32:54 +0300

    edit file

--- src/database/Functions/CheckLvl.ts
+++ src/database/Functions/CheckLvl.ts
@@ -10,7 +10,7 @@ export default async function CheckLvl(user: User, guild: Guild) {
     const userDb = await Users.findOne({ where: { user_id: user.id } });
 
     if (!userDb) return;
-
+    const oldExp = userDb.exp
     let currentExp = Number(userDb.exp);
     let currentLvl = Number(userDb.lvl);
     let gainedExp = currentExp; // Сохраняем изначальное количество опыта
@@ -32,7 +32,7 @@ export default async function CheckLvl(user: User, guild: Guild) {
     // Если уровень изменился, обновляем данные в базе
     if (currentLvl !== Number(userDb.lvl)) {
         userDb.lvl = currentLvl;
-        const oldExp = userDb.exp = gainedExp; // Сохраняем остаток опыта после всех повышений
+        userDb.exp = gainedExp; // Сохраняем остаток опыта после всех повышений
         userDb.need_exp = calculateRequiredExp(currentLvl); // Устанавливаем требование для следующего уровня
         userDb.balance = Number(userDb.balance) + Number(oldExp);
         await userDb.save();
